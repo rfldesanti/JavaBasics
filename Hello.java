533f157ac352f7555ab69547e2b48e6b303e8783
rfldesanti <rafaelsr2018@gmail.com>
2020-12-30 18:37:30 -0300

    Add if block

--- Hello.java
+++ Hello.java
@@ -2,9 +2,13 @@ class Hello {
     
     public static void main(String args[]){
 
-        int x = 4;        
-        System.out.println("Hello!");
-        System.out.println(x);
+        int x = 4;
+        if (x<3) {
+            System.out.println("Hello!");
+        } else {
+            System.out.println(x);
+        }      
+               
     }
 
 }
