cde7872369fb3ec1e41c79163753719c27af7302
rfldesanti <rafaelsr2018@gmail.com>
2020-12-30 18:39:46 -0300

    Add block while before block if

--- Hello.java
+++ Hello.java
@@ -3,12 +3,14 @@ class Hello {
     public static void main(String args[]){
 
         int x = 4;
-        if (x<3) {
-            System.out.println("Hello!");
-        } else {
-            System.out.println(x);
-        }      
-               
+        while (x>1) {
+            if (x>3){
+               System.out.println("Hello!");
+            } else {
+                System.out.println(x);
+            }      
+            x--;
+        }       
     }
 
 }
